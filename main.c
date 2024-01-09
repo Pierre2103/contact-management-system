e954c6dd7fbebe4096495f56815bdc24a2cc5aaf
David Cuahonte Cuevas <91249658+DavidCC812@users.noreply.github.com>
2024-01-09 09:59:53 +0100

    prepared prototyping

--- main.c
+++ main.c
@@ -3,9 +3,26 @@
 
 #define MAX_CONTACTS 100
 
+// Struct to represent a contact
 struct Contact {
     char name[50];
     char email[50];
     char phone[15];
     char category[20];
 };
+
+//Array to store contacts
+struct Contact contacts[MAX_CONTACTS];
+
+// Variable to Keep track of the number of contacts
+int numContacts = 0;
+
+//Function Prototyping
+void addContact();
+void modifyContact();
+void deleteContact();
+void searchContact();
+
+int main {
+
+}
